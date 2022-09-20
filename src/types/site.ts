81d5e672988abd160ce6a2747a37194e80d653cd
Aaron Pavlick <apavlick@yext.com>
2022-09-19 22:43:43 -0400

    merge conflict issues

--- src/types/site.ts
+++ src/types/site.ts
@@ -1,39 +1,7 @@
-<<<<<<< HEAD
 import { CategoryPhoto } from "./kg";
 
 interface Site {
   c_coverPhotos?: CategoryPhoto[];
-=======
-export interface ImageThumbnail {
-  url: string;
-  width: number;
-  height: number;
-}
-
-export interface Image {
-  url: string;
-  width: number;
-  height: number;
-  thumbnails?: ImageThumbnail[];
-  alternateText?: string;
-}
-
-export interface ComplexImage {
-  image: Image;
-  details?: string;
-  description?: string;
-  clickthroughUrl?: string;
-}
-
-export interface CategoryPhoto {
-  name: string;
-  photo: ComplexImage;
-  slug?: string;
-}
-
-interface Site {
-  c_coverPhoto?: ComplexImage;
->>>>>>> c30bded (home page)
   c_categoryPhotos?: CategoryPhoto[];
 }
 
