304e58d67ca934f50c9fba277e21bd716987ad45
Aaron Pavlick <apavlick@yext.com>
2022-10-04 14:51:18 -0400

    custom map pins

--- src/components/map/MapPin.tsx
+++ src/components/map/MapPin.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Transition } from "@headlessui/react";
 import { CardComponent, CardProps } from "@yext/search-ui-react";
 import Location from "../../types/locations";
+import PinIcon from "../../icons/PinIcon";
 
 // TODO: add to Pages module
 const MapPin: CardComponent<Location> = ({ result }: CardProps<Location>) => {
@@ -34,18 +35,10 @@ const MapPin: CardComponent<Location> = ({ result }: CardProps<Location>) => {
           </div>
         </div>
       </Transition>
-      <svg
+      <PinIcon
         onMouseEnter={() => setShowCard(true)}
         onMouseLeave={() => setShowCard(false)}
-        className="z-40 fill-slate-800 stroke-white stroke-2 hover:fill-slate-500"
-        width="33"
-        height="42"
-        viewBox="0 0 30 38"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path d="M30 15.0882C30 23.4212 23.3333 30.7353 15 38C7.22222 31.2941 0 23.4212 0 15.0882C0 6.75523 6.71573 0 15 0C23.2843 0 30 6.75523 30 15.0882Z" />
-      </svg>
+      />
     </div>
   );
 };
