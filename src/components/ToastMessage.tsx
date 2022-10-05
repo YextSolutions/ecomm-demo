859dd9ed4e6a9c6f5c6548851ea17d0909fb487d
Aaron Pavlick <apavlick@yext.com>
2022-10-05 12:06:33 -0400

    toast animation fix

--- src/components/ToastMessage.tsx
+++ src/components/ToastMessage.tsx
@@ -15,7 +15,7 @@ const ToastMessage = ({ message, onClose, show }: ToastMessageProps) => {
         show={show}
         enter="transform transition ease-in-out duration-500 sm:duration-700"
         enterFrom="translate-x-full"
-        enterTo="translate-x-16"
+        enterTo="translate-x-30"
         leave="transform transition ease-in-out duration-500 sm:duration-700"
         leaveFrom="translate-x-0"
         leaveTo="translate-x-full"
