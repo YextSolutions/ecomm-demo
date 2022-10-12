0d97ece5e9f5c247572a91d60db706daad7ba3f8
Aaron Pavlick <apavlick@yext.com>
2022-10-12 16:47:06 -0400

    removing cover photo from search pages

--- src/components/search/BeverageResults.tsx
+++ src/components/search/BeverageResults.tsx
@@ -1,39 +1,93 @@
 import * as React from "react";
-import { useSearchState, useSearchActions } from "@yext/search-headless-react";
+import { Pagination, ResultsCount } from "@yext/search-ui-react";
+import { useSearchState } from "@yext/search-headless-react";
+import { CategoryLink } from "../../types/kg";
+import { BreadcrumbsProps } from "../Breadcrumbs";
+import BeverageFilters from "./BeverageFilters";
+import BeverageResultsTitle from "./BeverageResultsTitle";
+import BeverageVerticalResults from "./BeverageVerticalResults";
 import { ShakerLoader } from "../ShakerLoader";
-import { useState } from "react";
-import BeverageResultsView from "./BeverageResultsView";
-import MobileFiltersView from "./mobile/MobileFiltersView";
+import TransitionContainer from "../TransitionContainer";
+import BottomButton from "../BottomButton";
+import SortingDropdown from "./SortingDropdown";
 
-interface BeverageResultsProps {
+interface BeverageResultsViewProps {
   title?: string;
+  breadcrumbs?: BreadcrumbsProps;
+  categories?: CategoryLink[];
+  bottomButtonOnClick: () => void;
 }
 
-const BeverageResults = ({ title }: BeverageResultsProps): JSX.Element => {
-  const [filtersOpen, setFiltersOpen] = useState(false);
-
+const BeverageResults = ({
+  title,
+  breadcrumbs,
+  categories,
+  bottomButtonOnClick,
+}: BeverageResultsViewProps): JSX.Element => {
+  const [showResults, setShowResults] = React.useState(false);
   const searchLoading = useSearchState((state) => state.searchStatus.isLoading);
 
-  const searchActions = useSearchActions();
-
-  const handleBottomButton = () => {
-    if (filtersOpen) {
-      searchActions.executeVerticalQuery();
+  // use effect hook that sets showResults to true 300 milliseconds after the search is done loading
+  React.useEffect(() => {
+    if (!searchLoading) {
+      const timeout = setTimeout(() => {
+        setShowResults(true);
+      }, 300);
+      return () => clearTimeout(timeout);
     }
-    setFiltersOpen(!filtersOpen);
-  };
+  }, [searchLoading]);
 
-  // TODO: Add NO_RESULTS view
-  // TODO: Clean up logic: https://twitter.com/DavidKPiano/status/1564950527477252098
   return (
     <>
-      {filtersOpen ? (
-        <MobileFiltersView bottomButtonOnClick={handleBottomButton} />
-      ) : searchLoading ? (
-        <ShakerLoader />
-      ) : (
-        <BeverageResultsView title={title} />
-      )}
+      {/* {coverPhoto && <CoverPhoto image={coverPhoto} />} */}
+      <div className="flex items-center justify-between pt-8">
+        <BeverageResultsTitle title={title} breadcrumbs={breadcrumbs} />
+        <TransitionContainer show={showResults}>
+          <SortingDropdown containerCss="hidden md:flex" />
+        </TransitionContainer>
+      </div>
+      <TransitionContainer show={showResults}>
+        <ResultsCount customCssClasses={{ resultsCountContainer: "pt-4" }} />
+      </TransitionContainer>
+      <div className="flex">
+        <TransitionContainer show={showResults}>
+          <div className="hidden pr-10 md:block">
+            <BeverageFilters
+              categories={categories}
+              standardFacetsProps={{
+                customCssClasses: {
+                  optionLabel: "font-bold whitespace-nowrap",
+                  optionInput: "text-orange focus:ring-orange ",
+                },
+                showMoreLimit: 5,
+              }}
+              numericalFacetProps={{
+                customCssClasses: {
+                  optionLabel: "font-bold whitespace-nowrap",
+                  optionInput: "text-orange focus:ring-orange ",
+                },
+              }}
+            />
+          </div>
+        </TransitionContainer>
+        {searchLoading ? (
+          <ShakerLoader />
+        ) : (
+          <TransitionContainer show={showResults}>
+            <div className="flex flex-col">
+              <BeverageVerticalResults />
+              {/* TODO: Scroll to top on click */}
+              <Pagination />
+            </div>
+          </TransitionContainer>
+        )}
+      </div>
+      <div className="block md:hidden">
+        <BottomButton
+          label={"SORT / FILTER"}
+          handleClick={bottomButtonOnClick}
+        />
+      </div>
     </>
   );
 };
