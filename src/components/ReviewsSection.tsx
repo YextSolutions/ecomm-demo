2d5aa907315b2774ca3e839776eaf1bd4d57badc
Aaron Pavlick <apavlick@yext.com>
2022-11-17 01:09:52 -0500

    reviews by count summary

--- src/components/ReviewsSection.tsx
+++ src/components/ReviewsSection.tsx
@@ -1,10 +1,15 @@
 import * as React from "react";
-import { useContext, useEffect } from "react";
-import { fetchReviews, ReviewsContext } from "./providers/ReviewsProvider";
+import { useContext, useEffect, useMemo } from "react";
+import {
+  fetchReviews,
+  fetchReviewTotals,
+  ReviewsContext,
+} from "./providers/ReviewsProvider";
 import { StarRating } from "./StarRating";
 import { v4 as uuid } from "uuid";
 import { formatDate } from "../util";
 import SortingDropdown from "./search/NewSortingDropdown";
+import { FaStar } from "react-icons/fa";
 
 interface ReviewSectionProps {
   entityId: string;
@@ -63,7 +68,13 @@ const ReviewsSection = ({
 }: ReviewSectionProps): JSX.Element => {
   const { reviewsState, dispatch } = useContext(ReviewsContext);
 
+  const countsByRating = useMemo(
+    () => reviewsState.countsByRating?.reverse(),
+    [reviewsState.countsByRating]
+  );
+
   useEffect(() => {
+    fetchReviewTotals(dispatch, entityId);
     fetchReviews(dispatch, entityId, 5);
   }, []);
 
@@ -97,36 +108,37 @@ const ReviewsSection = ({
             </p>
           </div>
 
-          {/* <div className="mt-6">
+          <div className="mt-6">
             <h3 className="sr-only">Review data</h3>
 
             <dl className="space-y-3">
-              {reviews.counts.map((count) => (
-                <div key={count.rating} className="flex items-center text-sm">
+              {countsByRating?.map((count, i) => (
+                <div key={uuid()} className="flex items-center text-sm">
                   <dt className="flex flex-1 items-center">
                     <p className="w-3 font-medium text-gray-900">
-                      {count.rating}
+                      {5 - i}
                       <span className="sr-only"> star reviews</span>
                     </p>
                     <div
                       aria-hidden="true"
                       className="ml-1 flex flex-1 items-center"
                     >
-                      <StarIcon
+                      <FaStar
                         className={classNames(
-                          count.count > 0 ? "text-yellow-400" : "text-gray-300",
-                          "h-5 w-5 flex-shrink-0"
+                          count > 0 ? "text-orange" : "text-gray-300",
+                          "h-4 w-4 flex-shrink-0"
                         )}
                         aria-hidden="true"
+                        key={uuid()}
                       />
 
                       <div className="relative ml-3 flex-1">
                         <div className="h-3 rounded-full border border-gray-200 bg-gray-100" />
-                        {count.count > 0 ? (
+                        {count > 0 ? (
                           <div
-                            className="absolute inset-y-0 rounded-full border border-yellow-400 bg-yellow-400"
+                            className="orange absolute inset-y-0 rounded-full border border-orange bg-orange"
                             style={{
-                              width: `calc(${count.count} / ${reviews.totalCount} * 100%)`,
+                              width: `calc(${count} / ${reviewsState.count} * 100%)`,
                             }}
                           />
                         ) : null}
@@ -134,13 +146,13 @@ const ReviewsSection = ({
                     </div>
                   </dt>
                   <dd className="ml-3 w-10 text-right text-sm tabular-nums text-gray-900">
-                    {Math.round((count.count / reviews.totalCount) * 100)}%
+                    {Math.round((count / reviewsState.count) * 100)}%
                   </dd>
                 </div>
               ))}
             </dl>
-          </div> */}
-
+          </div>
+          {/* 
           <div className="mt-10">
             <h3 className="text-lg font-medium text-gray-900">
               Share your thoughts
@@ -156,7 +168,7 @@ const ReviewsSection = ({
             >
               Write a review
             </a>
-          </div>
+          </div> */}
         </div>
 
         <div className="mt-16 lg:col-span-7 lg:col-start-6 lg:mt-0">
@@ -208,30 +220,3 @@ const ReviewsSection = ({
 };
 
 export default ReviewsSection;
-
-// import { StarIcon } from "@heroicons/react/20/solid";
-
-// const reviews = {
-//   average: 4,
-//   totalCount: 1624,
-//   counts: [
-//     { rating: 5, count: 1019 },
-//     { rating: 4, count: 162 },
-//     { rating: 3, count: 97 },
-//     { rating: 2, count: 199 },
-//     { rating: 1, count: 147 },
-//   ],
-//   featured: [
-//     {
-//       id: 1,
-//       rating: 5,
-//       content: `
-//         <p>This is the bag of my dreams. I took it on my last vacation and was able to fit an absurd amount of snacks for the many long and hungry flights.</p>
-//       `,
-//       author: "Emily Selman",
-//       avatarSrc:
-//         "https://images.unsplash.com/photo-1502685104226-ee32379fefbe?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=256&h=256&q=80",
-//     },
-//     // More reviews...
-//   ],
-// };
